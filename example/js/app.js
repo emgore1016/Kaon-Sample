24638a8fc4df28e09ce3660945327237f9ead0cf
Emily Gorelik <egorelik@geisel-software.com>
2022-10-09 17:55:56 -0400

    menu pops up (currently as a toggle)

--- example/js/app.js
+++ example/js/app.js
@@ -1,9 +1,16 @@
-$(document).ready(function(){
-	
-	$("[data-js-page]").on("touchend", function(){
-		var goal_page = $(this).data("js-page");
-		$(".page").removeClass("active");
-		$(".page[data-page='"+goal_page+"']").addClass("active");
-	})
+$(function () {
 
-})
+	$("[data-js-page]").on("touchend", function () {
+		$(".page").css("background", "red");
+
+		// var goal_page = $(this).data("js-page");
+		// $(".page").removeClass("active");
+		// $(".page[data-page='" + goal_page + "']").addClass("active");
+	});
+});
+
+$(function () {
+	$("#explore").on("touchend", function () {
+		$("#menu").css("background", "red");
+	});
+});
--- project/js/app.js
+++ project/js/app.js
@@ -1,9 +1,19 @@
-$(document).ready(function () {
+$(function () {
 
 	$("[data-js-page]").on("touchend", function () {
 		var goal_page = $(this).data("js-page");
 		$(".page").removeClass("active");
 		$(".page[data-page='" + goal_page + "']").addClass("active");
-	})
+	});
 
-})
+	$(".explore").on("touchend", function () {
+		if ($('#menu').css('opacity') == 1) {
+
+			$('#menu').css('opacity', '0');
+		}
+		else {
+			$('#menu').css('opacity', '1');
+		}
+		//$("#menu").fadeToggle(2000);
+	});
+});
